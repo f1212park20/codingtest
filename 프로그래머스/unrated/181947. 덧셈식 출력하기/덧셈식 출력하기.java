ceee71c68d4737a80511a80efb3ce6ba18582f47
park seung won <63566438+f1212park20@users.noreply.github.com>
2023-12-05 13:48:51 +0900

    [level unrated] Title: 덧셈식 출력하기, Time: 167.08 ms, Memory: 65.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181947. 덧셈식 출력하기/덧셈식 출력하기.java
+++ 프로그래머스/unrated/181947. 덧셈식 출력하기/덧셈식 출력하기.java
@@ -6,6 +6,6 @@ public class Solution {
         int a = sc.nextInt();
         int b = sc.nextInt();
 
-        System.out.printf("%s + %s = %s ",  a, b, (a+b));
+        System.out.printf("%d + %d = %d ",  a, b, (a+b));
     }
 }
