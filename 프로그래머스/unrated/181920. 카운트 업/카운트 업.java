4094a68d15fa361946ec15fae2b9278f18dca379
park seung won <63566438+f1212park20@users.noreply.github.com>
2023-12-23 11:23:53 +0900

    [level unrated] Title: 카운트 업, Time: 0.02 ms, Memory: 69.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181920. 카운트 업/카운트 업.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] solution(int start_num, int end_num) {
+        int size= end_num-start_num+1;
+		int[] answer = new int[size];
+		
+		int sum=0;
+		for(int i=0;i<size;i++) {
+			answer[i] = start_num++;
+			
+		}
+        
+        return answer;
+    }
+}
