24c3f303ffabb502808f9865b4c9fde33bcec760
park seung won <63566438+f1212park20@users.noreply.github.com>
2024-01-17 20:22:16 +0900

    [level 0] Title: 공백으로 구분하기 1, Time: 0.01 ms, Memory: 83.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181869. 공백으로 구분하기 1/공백으로 구분하기 1.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+
+class Solution {
+    public String[] solution(String my_string) {
+        
+        String[] answer = my_string.split(" ");
+        
+        return answer;
+    }
+}
