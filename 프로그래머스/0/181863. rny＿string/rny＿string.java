a06647be18d53ef1dc206a94c4de26a41aa78cb3
park seung won <63566438+f1212park20@users.noreply.github.com>
2024-01-30 20:06:55 +0900

    [level 0] Title: rny_string, Time: 0.02 ms, Memory: 78.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181863. rny＿string/rny＿string.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String rny_string) {
+        
+        
+        String str1=rny_string.replace("m", "rn");
+		String answer = str1;
+        
+        return answer;
+    }
+}
