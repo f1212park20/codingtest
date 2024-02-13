5c100ea66a0be8cfd22ded943ed790428d5884b4
park seung won <63566438+f1212park20@users.noreply.github.com>
2024-02-13 12:54:45 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.15 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(String my_string, int num1, int num2) {
+        String[] s= my_string.split("");     
+        
+        
+        String temp="";
+		temp=s[num1];
+		s[num1]=s[num2];
+		s[num2]=temp;
+        
+        String answer = String.join("",s);
+        
+        return answer;
+    }
+}
